00907aed01ccdaa0aa86182e64ef7966e165507c
linshenbo <linshenbo@didiglobal.com>
2020-09-07 17:50:22 +0800

    GAT init

---
+++ GAT/GraphAttentionLayer.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+class GraphAttentionLayer(keras.layers.Layer):
+    def __init__(self):
+        return 0
+
+    def build(self):
+        return 0
+
+    def call(self):
+        return 0
